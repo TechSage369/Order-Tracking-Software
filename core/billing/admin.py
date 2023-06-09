8c29fa82cf1ed7b4233492177063663da00f0dbf
TechSage369 <rainishchal9@gmail.com>
2023-06-09 18:55:45 +0530

    Refactor: Improve admin dashboard security and data integrity
    - Removed "OrderItem" from admin dashboard to prevent unauthorized manipulation of ordered items.
    - Disabled the ability to change the total of paid orders and delete paid orders from the admin dashboard.

--- core/billing/admin.py
+++ core/billing/admin.py
@@ -3,9 +3,22 @@ from django.contrib.auth.models import Group, User
 
 from .models import Item, Order, OrderItem
 
-admin.site.register(Order)
+
+class OrderAdminModel(admin.ModelAdmin):
+    # Add the fields you want to make read-only from the admin dashboard
+    readonly_fields = ('total_price', 'is_paid')
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # If editing an existing object
+            return self.readonly_fields  # Return the fields as read-only
+        return ()  # Otherwise, return an empty tuple to allow modifications
+
+    def has_delete_permission(self, request, obj=None):
+        return False  # Return False to disable the delete permission
+
+
+admin.site.register(Order, OrderAdminModel)
 admin.site.register(Item)
-admin.site.register(OrderItem)
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
